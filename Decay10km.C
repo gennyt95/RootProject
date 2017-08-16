f67816487e3bcf03ed359ce2a06d62efd187e468
gennyt95 <gennyt95@gmail.com>
2017-08-16 13:04:50 -0700

    Updated

--- Decay10km.C
+++ Decay10km.C
@@ -1,43 +1,56 @@
 # include <iostream>
 
+TH1F * makeLine(int uplim, int count) {
+    TH1F *vis = new TH1F("vis","Decay Histogram",100,0,uplim);
+    
+    if(count == 1) {
+        vis->SetLineColor(kRed);
+    } else if(count == 2){
+        vis->SetLineColor(kGreen);
+    } else if(count == 3) {
+        vis->SetLineColor(kBlue);
+    } else {
+        vis->SetLineColor(kBlack);
+    }
+    
+    return vis;
+}
+
 void setup(int uplim, const char* name,TCanvas *c1,int spot) {
-    // Makes stack for histograms
+    // Makes TF1s
     
     TF1 *hist100 = new TF1("hist100","exp(-0.01*x)",0.,uplim);
     TF1 *hist10 = new TF1("hist10","exp(-0.1*x)",0.,uplim);
     TF1 *hist1 = new TF1("hist1","exp(-1*x)",0.,uplim);
     TF1 *hist01 = new TF1("hist01","exp(-10*x)",0.,uplim);
     
-    // Sets up individual histograms
-    TH1F *vis = new TH1F("vis","Decay Histogram",100,0,uplim);
-    vis->SetLineColor(kRed);
-    TH1F *vis10 = new TH1F("vis10","Decay Histogram",100,0,uplim);
-    vis10->SetLineColor(kGreen);
-    TH1F *vis1 = new TH1F("vis1","Decay Histogram",100,0,uplim);
-    vis1->SetLineColor(kBlue);
-    TH1F *vis01 = new TH1F("vis01","Decay Histogram",100,0,uplim);
-    vis01->SetLineColor(kBlack);
+    // Makes lines
     
+    TH1F *lineOne = makeLine(uplim, 1);
+    lineOne->FillRandom("hist100", 100000);
     
-    // Fills histograms
-    vis->FillRandom("hist100", 100000);
-    vis10->FillRandom("hist10", 100000);
-    vis1->FillRandom("hist1", 100000);
-    vis01->FillRandom("hist01", 100000);
+    TH1F *lineTwo = makeLine(uplim, 2);
+    lineTwo->FillRandom("hist10", 100000);
     
-    // Adds to stack and draws
+    TH1F *lineThree = makeLine(uplim, 3);
+    lineThree->FillRandom("hist1", 100000);
+    
+    TH1F *lineFour = makeLine(uplim, 4);
+    lineFour->FillRandom("hist01", 100000);
+    
+    // Makes stack and draws
     THStack *result = new THStack("result",name);
     
-    result->Add(vis);
-    result->Add(vis10);
-    result->Add(vis1);
-    result->Add(vis01);
+    result->Add(lineOne);
+    result->Add(lineTwo);
+    result->Add(lineThree);
+    result->Add(lineFour);
     
     c1->cd(spot);
     result->Draw("nostack");
-    //return result;
     
 }
+
 
 void Decay10km() {
     TCanvas *c1 = new TCanvas("c1","c1");
